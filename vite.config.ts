723da155e82433165109b62ea525819f3e7f0d48
Ignatus <ignatusa3@gmail.com>
2025-04-12 16:40:03 +0000

    Update configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',  // Replace 'portfolio' with your repository name
+  base: '/portfolio-devops/',  // Replace 'portfolio' with your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },

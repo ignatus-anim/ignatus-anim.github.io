0a5929fdad1b24f42f93764c115deb61ac92aec7
Ignatus <ignatusa3@gmail.com>
2025-04-12 16:36:31 +0000

    updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/',  // Replace 'portfolio' with your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },

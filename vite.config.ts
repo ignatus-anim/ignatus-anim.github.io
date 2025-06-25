a5ca3a5765c94cf862579f0c0c636487e78fd61f
Ignatus <ignatusa3@gmail.com>
2025-06-25 22:27:57 +0000

    chore: add repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '',  // Updated to match your repository name
+  base: 'ignatus-anim.github.io',  // Updated to match your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
